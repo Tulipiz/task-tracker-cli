9aa3dd425c154afc53c4a0343b112763edebb029
Luiz Felipe <luiz.dias@meta.com.br>
2025-03-31 11:29:42 -0300

    ajuste na escolhe de opções, ajustes na descrição do metodo ADD e criação do metodo UPDATE

--- task-tracker-input.php
+++ task-tracker-input.php
@@ -17,15 +17,20 @@ $menu = [
         echo "\033c";
     },
     'add' => function ($args) use ($filePath) {
+        $description = trim($args, "\"");
+        if (empty(trim($description))) {
+            echo "Erro: A descrição não pode estar vazia!\n";
+            return;
+        }
         $data = [
             "id" => 1,
-            "description" => $args,
+            "description" => $description,
             "status" => "done",
             "createdAT" =>  date('d/m/Y H:i:s'),
             "updatedAt" => null
         ];
 
-        if(file_exists($filePath) && filesize($filePath) > 0){
+        if (file_exists($filePath) && filesize($filePath) > 0) {
             $tasks = file_get_contents($filePath);
             $tasksArray = json_decode($tasks, true);
         } else {
@@ -33,19 +38,50 @@ $menu = [
         }
 
         $lastId = !empty($tasksArray) ? end($tasksArray)['id'] : 0;
-
         $newId = $lastId + 1;
-
         $data['id'] = $newId;
 
         $tasksArray[] = $data;
 
         $json = json_encode($tasksArray, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
-        if(file_put_contents($filePath, $json)){
+        if (file_put_contents($filePath, $json)) {
             echo "Task added successfully (ID:" . $data['id'] . ")\n";
         }
     },
+    'update' => function ($id, $args) use ($filePath) {
+        $data = json_decode(file_get_contents($filePath), true);
+        $description = trim($args, "\"");
+        if (empty(trim($description))) {
+            echo "Erro: A descrição não pode estar vazia!\n";
+            return;
+        }
+
+        if (!$data) {
+            echo "erro ao ler o arquivo JSON.\n";
+            return;
+        }
+
+        $userFound = false;
+        foreach ($data as &$task) {
+            if ($task['id'] === $id) {
+                $task['description'] = $description;
+                $task['updatedAt'] = date('d/m/Y H:i:s');
+                $userFound = true;
+                break;
+            }
+        }
+  
+        if($userFound){
+            $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents($filePath, $json);
+            echo "Task added successfully (ID:" . $id . ")\n";
+        } else {
+            echo "Usuário não encontrado.\n";
+        }
+
+    }
+
 ];
 echo ("\n\n Digite 'help' para listar todos os comandos disponiveis!\n\n");
 
@@ -53,14 +89,28 @@ while (true) {
     echo "task-cli ";
     $input = trim(fgets(STDIN));
 
-    $parts = explode(" ", $input);
+    $specialCommands = ['exit', 'help', 'clear'];
+    if (in_array($input, $specialCommands)) {
+        $menu[$input]();
+        continue;
+    }
 
-    $command = array_shift($parts);
-    $args = implode(" ", $parts);
+    preg_match('/^(\w+)\s+(\d+)?\s*"([^"]+)"/', $input, $matches);
+
+    $command = $matches[1] ?? null;
+    $id = isset($matches[2]) ? (int)$matches[2] : null;
+    $args = $matches[3] ?? null;
 
     if (isset($menu[$command])) {
-        $menu[$command]($args);
+
+        if ($command === 'add') {
+            $menu[$command]($args);
+        } elseif ($command === 'update' && $id) {
+            $menu[$command]($id, $args);
+        } else {
+            echo "Erro: ID da tarefa é necessário para o comando update!\n";
+        }
     } else {
-        echo "Opção Invalida";
+        echo "Opção Inválida\n";
     }
 }
